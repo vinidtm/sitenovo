cde35cb946b7d03b1a4ca717f249b46d7898bd4e
Vinidtm <vinidtm@hotmail.com>
2023-09-11 13:59:36 -0300

    cl

--- pages/cl.js
+++ pages/cl.js
@@ -13,7 +13,7 @@ const P1 = () => {
     <link rel='shortcut icon' href='/Icon.jpg' />
     <script>
           {`
-            var urlBackRedirect = 'https://saudeperfeitahoje.online/promocao';
+            var urlBackRedirect = 'https://saudeperfeitahoje.online/promocaocl';
             urlBackRedirect = urlBackRedirect.trim() +
                 (urlBackRedirect.indexOf("?") > 0 ? '&' : '?') +
                 document.location.search.replace('?', '').toString();
