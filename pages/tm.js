fe13b66c29e25e4b0905e2dace53779c1e9dad47
Vinidtm <vinidtm@hotmail.com>
2023-09-11 14:44:56 -0300

    truquem

---
+++ pages/tm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Footer from './footer';
+import Head from 'next/head';
+import Videocl from './videocl';
+import Comentariotm from './comentariotm';
+
+
+const P1 = () => {
+  return (
+    <>
+    <Head>
+    <title>Saúde Perfeita</title>
+    <link rel='shortcut icon' href='/Icon.jpg' />
+    <script>
+          {`
+            var urlBackRedirect = 'https://saudeperfeitahoje.online/promocaotm';
+            urlBackRedirect = urlBackRedirect.trim() +
+                (urlBackRedirect.indexOf("?") > 0 ? '&' : '?') +
+                document.location.search.replace('?', '').toString();
+            history.pushState({}, "", location.href);
+            history.pushState({}, "", location.href);
+            window.onpopstate = function () {
+                setTimeout(function () {
+                    location.href = urlBackRedirect;
+                }, 1);
+            };
+          `}
+        </script>
+<link rel="preload" href="https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/64ff384502d65b000a08dd76/player.js" as="script"/>
+<link rel="preload" href="https://cdn.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script"/>
+<link rel="preload" href="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/64ff384502d65b000a08dd76/thumbnail.jpg" as="image"/>
+<link rel="preload" href="https://cdn.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/64ff382c5d415700094383c5/main.m3u8" as="fetch"/>
+<link rel="dns-prefetch" href="https://cdn.converteai.net"/>
+<link rel="dns-prefetch" href="https://scripts.converteai.net"/>
+<link rel="dns-prefetch" href="https://images.converteai.net"/>
+<link rel="dns-prefetch" href="https://api.vturb.com.br"/>
+    </Head>
+    <div>
+    <div className='video-player-container'>
+    <Videocl/>
+    </div>
+    <Comentariotm/>
+    <Footer />
+    </div>
+    </>
+  );
+};
+
+export default P1;
