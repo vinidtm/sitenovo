175e85ab4a2323b7f9365462d0baed1c17967a10
Vinidtm <vinidtm@hotmail.com>
2023-10-18 11:42:26 -0300

    head

---
+++ pages/hn.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Footer from './footer';
+import Head from 'next/head';
+import Comentariopv1 from './comentariopv1';
+import Videohn from './videohn';
+
+const Hn = () => {
+  return (
+    <>
+    <Head>
+    <title>Saúde Perfeita</title>
+    <link rel='shortcut icon' href='/Icon.jpg' />
+    <script>
+          {`
+            var urlBackRedirect = 'https://saudeperfeitahoje.online/promocaopv1';
+            urlBackRedirect = urlBackRedirect.trim() +
+                (urlBackRedirect.indexOf("?") > 0 ? '&' : '?') +
+                document.location.search.replace('?', '').toString();
+            history.pushState({}, "", location.href);
+            history.pushState({}, "", location.href);
+            window.onpopstate = function () {
+                setTimeout(function () {
+                    location.href = urlBackRedirect;
+                }, 1);
+            };
+          `}
+        </script>
+
+<link rel="preload" href="https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/652dc34892b09e000852d614/player.js" as="script"/>
+<link rel="preload" href="https://cdn.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script"/>
+<link rel="preload" href="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/652dc34892b09e000852d614/thumbnail.jpg" as="image"/>
+<link rel="preload" href="https://cdn.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/652dc32afb781d0009e6bd6a/main.m3u8" as="fetch"/>
+<link rel="dns-prefetch" href="https://cdn.converteai.net"/>
+<link rel="dns-prefetch" href="https://scripts.converteai.net"/>
+<link rel="dns-prefetch" href="https://images.converteai.net"/>
+<link rel="dns-prefetch" href="https://api.vturb.com.br"/>
+    </Head>
+    <div>
+    <div className='video-player-container'>
+    <Videohn/>
+    </div>
+    <Comentariopv1/>
+    <Footer />
+    </div>
+    </>
+  );
+};
+
+export default Hn;
