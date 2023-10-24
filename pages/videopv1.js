c2fb51b0cabba2b7dd012e0678d025cfcc0f3ec6
Vinidtm <vinidtm@hotmail.com>
2023-10-24 20:12:42 -0300

    v

--- pages/videopv1.js
+++ pages/videopv1.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 
-function videocl () {
+function video01 () {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/64ff13f2e1c7560009bd8dd8/player.js";
+    script.src = "https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/65382eefe956e500094cdb48/player.js";
     script.async = true;
     document.head.appendChild(script);
 
@@ -14,13 +14,13 @@ function videocl () {
   }, []);
 
   return (
-    <div id="vid_64ff13f2e1c7560009bd8dd8" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
-      <img id="thumb_64ff13f2e1c7560009bd8dd8" src="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/64ff13f2e1c7560009bd8dd8/thumbnail.jpg" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block' }} />
-      <div id="backdrop_64ff13f2e1c7560009bd8dd8" style={{ position: 'absolute', top: 0, width: '100%', height: '100%', WebkitBackdropFilter: 'blur(5px)', backdropFilter: 'blur(5px)' }}></div>
+    <div id="vid_65382eefe956e500094cdb48" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
+      <img id="thumb_65382eefe956e500094cdb48" src="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/65382eefe956e500094cdb48/thumbnail.jpg" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block' }} />
+      <div id="backdrop_65382eefe956e500094cdb48" style={{ position: 'absolute', top: 0, width: '100%', height: '100%', WebkitBackdropFilter: 'blur(5px)', backdropFilter: 'blur(5px)' }}></div>
     </div>
   );
 }
 
 
 
-export default videocl ;
+export default video01 ;
