5c7f59336d1526db2c4388fb85999947abe8f1c8
Vinidtm <vinidtm@hotmail.com>
2024-02-22 20:15:23 -0300

    noval

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Video01 from './video01';
 import doutorfoto from './fotos/DrMarcoscirculo.png';
 import Image from 'next/image';
+import { Helmet } from "react-helmet"
 
 
 const Home = () => {
@@ -28,10 +29,9 @@ const Home = () => {
           `}
         </script>
 
-<link rel="preload" href="https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/65b936a78621960008939330/player.js" as="script"/>
+<link rel="preload" href="https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/ab-test/655fadec01dd3c002efe655b/player.js" as="script"/>
 <link rel="preload" href="https://cdn.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script"/>
-<link rel="preload" href="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/65b936a78621960008939330/thumbnail.jpg" as="image"/>
-<link rel="preload" href="https://cdn.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/63ade75522206d000a63c834/main.m3u8" as="fetch"/>
+<link rel="preload" href="https://images.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/players/655fadec01dd3c002efe655b/thumbnail.jpg" as="image"/>
 <link rel="dns-prefetch" href="https://cdn.converteai.net"/>
 <link rel="dns-prefetch" href="https://scripts.converteai.net"/>
 <link rel="dns-prefetch" href="https://images.converteai.net"/>
@@ -40,20 +40,11 @@ const Home = () => {
     </Head>
     <div>
     <div className='video-player-container'>
-    <Video01/>
+    <div dangerouslySetInnerHTML={{ __html: '<script type="text/javascript" id="scr_65d7ca9b97098c0007bd9817"></script>' }} />
+      <Helmet>
+      <script type="text/javascript">var s=document.createElement("script");s.src="https://scripts.converteai.net/1db8e03a-c1fc-4fa6-b094-4a5346a615e6/ab-test/65d7ca9b97098c0007bd9817/player.js",s.async=!0,document.head.appendChild(s);</script>
+      </Helmet>
     </div>
-    <div className='doutor'>
-  <Image
-    src={doutorfoto}
-    alt='Dr. Marcos'
-    width={250}
-    height={250}
-  />
-  <div className='texto-doutor'>
-    <h2>Dr. Marcos Camargo </h2>
-    <p> Cientista e pesquisador especialista no emagrecimento natural a mais de 10 anos, profissional dedicado e que já transformou a vida de milhares de pessoas que sofriam constantemente para perder peso. </p>
-    <p> Com uma historia pessoal de superação enfrentando e vencendo sua própria luta contra o sobrepeso, ele compreende profundamente a batalha contra a balança e divulga como passar por isso de forma natural e saudável com uma simples receita caseira!</p>   </div>
-        </div>
     <Footer />
     </div>
     </>
